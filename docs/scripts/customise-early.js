c903a6d4cd09054b8e858dbf596456ff0d6ec781
Elttob <dan.bottlefact@gmail.com>
2020-12-20 16:47:28 +0000

    Dark theme for index page

--- docs/scripts/customise-early.js
+++ docs/scripts/customise-early.js
@@ -1,7 +1,11 @@
-let preferredTheme = "platinum";
-
-if(window.matchMedia != null && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-	preferredTheme = "graphite";
+function updateFavicon(isDark) {
+	document.documentElement.className = "theme-" + (isDark ? "graphite" : "platinum");
 }
 
-document.documentElement.className = "theme-" + preferredTheme;
+if(window.matchMedia != null) {
+	let themeMedia = window.matchMedia('(prefers-color-scheme: dark)');
+	updateFavicon(themeMedia.matches);
+
+	themeMedia.addListener(media => {updateFavicon(media.matches);});
+}
+
