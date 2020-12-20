a064eda5caf7658af34c43eeeba2cf199a8f6b0a
Elttob <dan.bottlefact@gmail.com>
2020-12-20 16:56:47 +0000

    Fix customiser theme

--- docs/scripts/customise-early.js
+++ docs/scripts/customise-early.js
@@ -1,11 +1,7 @@
-function updateFavicon(isDark) {
-	document.documentElement.className = "theme-" + (isDark ? "graphite" : "platinum");
+let preferredTheme = "platinum";
+
+if(window.matchMedia != null && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	preferredTheme = "graphite";
 }
 
-if(window.matchMedia != null) {
-	let themeMedia = window.matchMedia('(prefers-color-scheme: dark)');
-	updateFavicon(themeMedia.matches);
-
-	themeMedia.addListener(media => {updateFavicon(media.matches);});
-}
-
+document.documentElement.className = "theme-" + preferredTheme;
