800962e89e5faeb6ef058e15d5367017236e9e49
Elttob <dan.bottlefact@gmail.com>
2022-09-25 12:54:36 +0100

    Vanilla 3 release

--- docs/scripts/customise-early.js
+++
@@ -1,7 +0,0 @@
-let preferredTheme = "platinum";
-
-if(window.matchMedia != null && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-	preferredTheme = "graphite";
-}
-
-document.documentElement.className = "theme-" + preferredTheme;
